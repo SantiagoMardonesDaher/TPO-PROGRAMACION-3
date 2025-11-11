490623d4595d471e43b71b313fc98d6ed1bbedca
SantiagoMardonesDaher <163934892+SantiagoMardonesDaher@users.noreply.github.com>
2025-11-10 22:32:50 -0300

    Refactor Mochila01DP to use Objeto class
    Refactor mochila algorithm to use Objeto class and improve readability.

--- Mochila01DP.java
+++ Mochila01DP.java
@@ -1,75 +1,94 @@
 package progra3.TPO_PROGRAMACION_MARDONES;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mochila01DP {
 
-    static int mochilaConReconstruccion(int n, int W, int[] w, int[] v, int[] tomadosOut){
-        int[][] dp = new int[n+1][W+1];
+    static class Objeto {
+        int peso;
+        int valor;
+        String nombre;
 
-        for(int i=1; i<=n; i++){
-            for(int c=0; c<=W; c++){
-                if(w[i] > c){
-                    dp[i][c] = dp[i-1][c];
-                }else{
-                    int sin_i = dp[i-1][c];
-                    int con_i = dp[i-1][c - w[i]] + v[i];
-                    dp[i][c] = (con_i > sin_i) ? con_i : sin_i;
+        public Objeto(String nombre, int peso, int valor) {
+            this.nombre = nombre;
+            this.peso = peso;
+            this.valor = valor;
+        }
+    }
+
+    public static int mochila(List<Objeto> objetos, int capacidad) {
+        int n = objetos.size();
+        int[][] M = new int[n][capacidad + 1]; 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= capacidad; j++) {
+
+                if (i == 0) {
+                    if (objetos.get(i).peso <= j)
+                        M[i][j] = objetos.get(i).valor;
+                    else
+                        M[i][j] = 0;
+                } else {
+                    if (objetos.get(i).peso > j) {
+                        M[i][j] = M[i - 1][j];
+                    } else {
+                        int noTomar = M[i - 1][j];
+                        int tomar = M[i - 1][j - objetos.get(i).peso] + objetos.get(i).valor;
+                        M[i][j] = Math.max(noTomar, tomar);
+                    }
                 }
             }
         }
 
+        List<Objeto> seleccionados = new ArrayList<>();
+        int i = n - 1;
+        int j = capacidad;
 
-        int c = W;
-        for(int i=n; i>=1; i--){
-            if(dp[i][c] != dp[i-1][c]){       
-                tomadosOut[i] = 1;
-                c -= w[i];
-            }else{
-                tomadosOut[i] = 0;
+        while (i >= 0 && j >= 0) {
+            if (i == 0) {
+                if (M[i][j] != 0)
+                    seleccionados.add(objetos.get(i));
+                break;
+            }
+
+            if (M[i][j] != M[i - 1][j]) {
+                seleccionados.add(objetos.get(i));
+                j -= objetos.get(i).peso;
+                i--;
+            } else {
+                i--;
             }
         }
-        return dp[n][W];
-    }
 
-    static int mochilaValorSolo(int n, int W, int[] w, int[] v){
-        int[] dp = new int[W+1];
-        for(int i=1; i<=n; i++){
-            for(int c=W; c>=w[i]; c--){
-                int con_i = dp[c - w[i]] + v[i];
-                if(con_i > dp[c]) dp[c] = con_i;
-            }
+        System.out.println("\nObjetos seleccionados:");
+        for (int k = seleccionados.size() - 1; k >= 0; k--) {
+            Objeto o = seleccionados.get(k);
+            System.out.println("- " + o.nombre + " (peso: " + o.peso + ", valor: " + o.valor + ")");
         }
-        return dp[W];
+
+        return M[n - 1][capacidad];
     }
 
-    //pruebas 
-    static void imprimirSeleccion(int[] tomados){
-        System.out.print("Objetos tomados: ");
-        for(int i=1;i<tomados.length;i++) if(tomados[i]==1) System.out.print(i+" ");
-        System.out.println();
-    }
+    // Método principal 
+    public static void main(String[] args) {
+        List<Objeto> objetos = new ArrayList<>();
 
-    public static void main(String[] args){
-        int n = 3, W = 10;
-        int[] w = new int[]{0, 6, 3, 4};  // w[1]=6, w[2]=3, w[3]=4
-        int[] v = new int[]{0, 30,14,16}; // v[1]=30, v[2]=14, v[3]=16
+        objetos.add(new Objeto("O1", 6, 30));
+        objetos.add(new Objeto("O2", 3, 14));
+        objetos.add(new Objeto("O3", 4, 16));
 
-        int[] tomados = new int[n+1];
-        int valor = mochilaConReconstruccion(n, W, w, v, tomados);
-        System.out.println("Valor óptimo = " + valor);    
-        imprimirSeleccion(tomados);                             
+        int capacidad = 10;
 
-        // Prueba extra
-        int n2=4, W2=7;
-        int[] w2 = new int[]{0,2,3,4,5};
-        int[] v2 = new int[]{0,6,8,7,10};
-        int[] toma2 = new int[n2+1];
-        int val2 = mochilaConReconstruccion(n2, W2, w2, v2, toma2);
-        System.out.println("Valor óptimo = " + val2);
-        imprimirSeleccion(toma2);
+        System.out.println("Capacidad máxima de la mochila: " + capacidad);
+        System.out.println("Objetos disponibles:");
+        for (Objeto o : objetos) {
+            System.out.println("- " + o.nombre + " (peso: " + o.peso + ", valor: " + o.valor + ")");
+        }
 
-      
-        int valSolo = mochilaValorSolo(n2, W2, w2, v2);
-        System.out.println("Valor óptimo (solo valor) = " + valSolo);
+        int valorMaximo = mochila(objetos, capacidad);
+
+        System.out.println("\nValor máximo obtenido: " + valorMaximo);
     }
 }
 
